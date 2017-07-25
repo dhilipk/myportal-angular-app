2d58baede4f0b81c6a0b5fe60f9302b8a56c81a8
deswaran <deswaran@paypal.com>
2017-07-25 06:30:11 -0400

    Added Bootstrap component to angular2.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { AppComponent } from './app.component';
+import { SigninComponent } from './signin/signin.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SigninComponent
   ],
   imports: [
+    NgbModule.forRoot(),
     BrowserModule
   ],
   providers: [],
---
+++ src/app/mock-city.ts
@@ -0,0 +1,13 @@
+import { City } from './city';
+
+export const ORIGINS: City[] = [
+  {id: 1, name: 'Chennai', code: 'MAA'},
+  {id: 2, name: 'Pune', code: 'PNQ'},
+  {id: 3, name: 'Mumbai', code: 'BOM'}
+];
+
+export const DESTINATIONS: City[] = [
+  {id: 4, name: 'Bangalore', code: 'BLR'},
+  {id: 5, name: 'Kolkatta', code: 'CCU'},
+  {id: 6, name: 'Delhi', code: 'DEL'}
+];
